fa1393eac6480d14741237cda90ddf3ef336ad19
eugenio-cyber <wesleyeugenio3@gmail.com>
2021-12-29 15:41:46 -0300

    Finalizacao do Java Script, CSS e HTML

--- index.js
+++ index.js
@@ -5,39 +5,145 @@ function calc() {
     const nome = document.getElementById("nome").value;
     const peso = document.getElementById("peso").value;
     const altura = document.getElementById("altura").value;
-    const modal = document.getElementById("modal-erro");
+    const moderro = document.getElementById("modal-erro");
+    const modresult = document.getElementById("modal-resultado");
     var select = document.getElementById("sexo");
     var value = select.options[select.selectedIndex].value;
 
-    if(nome !== "" && peso !== "" && altura !== "" && value) {
+    if(nome !== "" && peso !== "" && altura !== "" && value !== "vazio") {
 
         const IMC = (peso / (altura * altura)).toFixed(1);
 
         if(IMC < 18.5) {
 
-            alert("MAGREZA")
+            if(modresult) {
+
+                const nome2 = document.getElementById("nome2");
+                const resultado = document.getElementById("resultado");
+                resultado.innerHTML = "Magreza";
+
+                modresult.classList.add("mostrar");
+                modresult.addEventListener("click", (e) => {
+
+                    if(e.target.id == "modal-erro" || e.target.classList == "fechar") {
+
+                        modresult.classList.remove("mostrar");
+                        window.location.reload()
+
+                    }
+                    
+                })
+
+            }
 
         } else if(IMC >= 18.5 && IMC <= 24.9) {
 
-            alert("NORMAL")
+            if(modresult) {
+
+                const nome2 = document.getElementById("nome2");
+                const resultado = document.getElementById("resultado");
+                resultado.innerHTML = "Normal";
+
+                modresult.classList.add("mostrar");
+                modresult.addEventListener("click", (e) => {
+
+                    if(e.target.id == "modal-erro" || e.target.classList == "fechar") {
+
+                        modresult.classList.remove("mostrar");
+                        window.location.reload()
+
+                    }
+                    
+                })
+
+            }
 
         } else if(IMC >= 25.0 && IMC <= 29.9) {
 
-            alert("SOBREPESO")
+            if(modresult) {
+
+                const nome2 = document.getElementById("nome2");
+                const resultado = document.getElementById("resultado");
+                resultado.innerHTML = "Sobrepeso";
+
+                modresult.classList.add("mostrar");
+                modresult.addEventListener("click", (e) => {
+
+                    if(e.target.id == "modal-erro" || e.target.classList == "fechar") {
+
+                        modresult.classList.remove("mostrar");
+                        window.location.reload()
+
+                    }
+                    
+                })
+
+            }
 
         } else if(IMC >= 30.0 && IMC <= 39.9) {
 
-            alert("OBESIDADE")
+            if(modresult) {
+
+                const nome2 = document.getElementById("nome2");
+                const resultado = document.getElementById("resultado");
+                resultado.innerHTML = "Obesidade";
+
+                modresult.classList.add("mostrar");
+                modresult.addEventListener("click", (e) => {
+
+                    if(e.target.id == "modal-erro" || e.target.classList == "fechar") {
+
+                        modresult.classList.remove("mostrar");
+                        window.location.reload()
+
+                    }
+                    
+                })
+
+            }
 
         } else if(IMC > 40.0) {
 
-            alert("OBESIDADE GRAVE")
+            if(modresult) {
+
+                const nome2 = document.getElementById("nome2");
+                const resultado = document.getElementById("resultado");
+                resultado.innerHTML = "Obesidade Grave";
+
+                modresult.classList.add("mostrar");
+                modresult.addEventListener("click", (e) => {
+
+                    if(e.target.id == "modal-erro" || e.target.classList == "fechar") {
+
+                        modresult.classList.remove("mostrar");
+                        window.location.reload()
+
+                    }
+                    
+                })
+
+            }
 
         }
 
     } else {
         
-        modal.classList.add("mostrar");
+        if(moderro) {
+
+            const nn = document.getElementById("nome2");
+            nn.innerHTML = nome;
+
+            moderro.classList.add("mostrar");
+            moderro.addEventListener("click", (e) => {
+
+                if(e.target.id == "modal-erro" || e.target.classList == "fechar") {
+
+                    moderro.classList.remove("mostrar");
+
+                }
+                
+            })
+        }
 
     }
 }
